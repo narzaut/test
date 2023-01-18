27e208299a12009cd6a041bae785eb72aa20dc4d
Ignacio Arzaut <ignacio.arzaut@surfdevelopment.tech>
2023-01-17 23:50:00 -0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 //* This line adds 'requireFromRoot' to global object and makes it usable from anywhere in the code
 const express = require('express');
-const dns = require('dns');
+const isUrl = require('valid-url');
 
 const cors = require('cors');
 
@@ -20,19 +20,15 @@ let urlIndex = 1;
 const urlMap = new Map();
 
 app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body;
-  const host = new URL(url).host;
-
-  dns.lookup(host, (err) => {
-    if (err) {
-      return res.json({ error: 'invalid url' });
+    const { url } = req.body;
+    if (!isUrl.isUri(url)) {
+        return res.json({ error: 'invalid url' });
     }
-
+  
     urlMap.set(urlIndex, url);
     res.json({ original_url: url, short_url: urlIndex });
     urlIndex++;
   });
-});
 
 app.get('/api/shorturl/:index', (req, res) => {
   const { index } = req.params;
