ebdfa7475bcad6bf1dd1e79bf351918478374429
Ignacio Arzaut <ignacio.arzaut@surfdevelopment.tech>
2023-01-18 00:01:32 -0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 //* This line adds 'requireFromRoot' to global object and makes it usable from anywhere in the code
 const express = require('express');
-const isUrl = require('valid-url');
+const url = require('url');
 const shortid = require('shortid');
 const cors = require('cors');
 
@@ -16,28 +16,30 @@ const corsOptions = {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
+
 const urlDb = {};
 
+
 app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body;
-  if (!isUrl.isUri(url)) {
+    const { url } = req.body;
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol !== 'http:' && parsedUrl.protocol !== 'https:') {
       return res.json({ error: 'invalid url' });
-  }
-
-  const shortUrl = shortid.generate();
-  urlDb[shortUrl] = url;
-  res.json({ original_url: url, short_url: shortUrl });
-});
-
-app.get('/api/shorturl/:shortUrl', (req, res) => {
-  const { shortUrl } = req.params;
-  if (!urlDb[shortUrl]) {
-    return res.json({ error: 'invalid short url' });
-  }
-
-  res.redirect(urlDb[shortUrl]);
-});
-
+    }
+  
+    const shortUrl = shortid.generate();
+    urlDb[shortUrl] = url;
+    res.json({ original_url: url, short_url: shortUrl });
+  });
+  
+  app.get('/api/shorturl/:shortUrl', (req, res) => {
+    const { shortUrl } = req.params;
+    if (!urlDb[shortUrl]) {
+      return res.json({ error: 'invalid url' });
+    }
+  
+    res.redirect(urlDb[shortUrl]);
+  });
 app.listen(4123, () => {
     console.log(`Listening to port 4123`);
 });
